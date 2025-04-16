a05092b514e7c1f336d9c01dd5ebfdab9fef9691
Andrzej Ludwikowski <andrzej.ludwikowski@lightbend.com>
2025-04-16 13:45:00 +0200

    cleaning the code

--- src/main/java/io/akka/ai/application/AIClient.java
+++ src/main/java/io/akka/ai/application/AIClient.java
@@ -4,6 +4,9 @@ import io.akka.tagging.domain.TaggingResult;
 
 import java.util.concurrent.CompletionStage;
 
+/**
+ * Interface for AI client to interact with an AI service.
+ */
 public interface AIClient {
-  CompletionStage<TaggingResult> call(String input);
+    CompletionStage<TaggingResult> call(String input);
 }
